2fd44020c3cb806d32034d2517b577865cd43f51
mig-af <mkmouse786@gmail.com>
2025-06-12 16:51:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ api.add_resource(ResumenController, "/api", "/api/<int:date>")
 
 if __name__ == '__main__':
     
-    app.run(port= 4000, debug=True)
+    app.run(host='0.0.0.0',port= 4000, debug=True)
